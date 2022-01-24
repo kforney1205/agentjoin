dcd143f97de53e4b335768207e2d65d03f5d81bf
kforney1205 <kyle.forney@gmail.com>
2022-01-23 18:24:48 -0600

    updates

--- js/urlGenerate.js
+++ js/urlGenerate.js
@@ -1,7 +1,7 @@
 
 function urlGen(f){
-   var i1 = 'urlSeg1';
-   var i2 = f.intranet.value;
+   var i1 = 'https://xmb-hc.pex-gcc.com/webapp/#/?conference=hcp';
+   var i2 = document.getElementById('videoID').value;
    var i3 = 'urlSef';
    f.action = i1 + i2 + i3;
    return true;
