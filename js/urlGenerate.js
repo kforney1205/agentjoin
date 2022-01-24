b4229bc40f446ae43f40aef73434291abf035c2e
kforney1205 <kyle.forney@gmail.com>
2022-01-23 19:06:22 -0600

    changes

--- js/urlGenerate.js
+++ js/urlGenerate.js
@@ -1,8 +1,7 @@
 
 function urlGen(f){
-   var i1 = 'https://xmb-hc.pex-gcc.com/webapp/#/?conference=hcp';
+   var i1 = 'https://kforney1205.github.io/kiosk?conference=mp';
    var i2 = document.getElementById('videoID').value;
-   var i3 = 'urlSef';
-   f.action = i1 + i2 + i3;
+   f.action = i1 + i2;
    return true;
 }
