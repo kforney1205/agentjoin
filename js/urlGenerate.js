d9f64e19f7ef7a26a5156a9b5c1c330b1d84cb06
kforney1205 <kyle.forney@gmail.com>
2022-01-24 17:09:12 -0600

    Update urlGenerate.js

--- js/urlGenerate.js
+++ js/urlGenerate.js
@@ -1,7 +1,7 @@
 
 function urlGen(f){
    var i1 = 'https://kforney1205.github.io/agentjoin/kiosk.html?';
-   var i2 = document.getElementById('videoID').value;
+   var i2 = "mp" + document.getElementById('videoID').value;
    f.action = i1 + i2;
    return true;
 }
