afc64d5f60c7ee8c224933106b2d690bf7a4ff53
kforney1205 <kyle.forney@gmail.com>
2022-01-24 17:03:57 -0600

    djfhd

--- js/urlGenerate.js
+++ js/urlGenerate.js
@@ -1,6 +1,6 @@
 
 function urlGen(f){
-   var i1 = 'https://kforney1205.github.io/agentjoin/kiosk.html?conference=mp';
+   var i1 = 'https://kforney1205.github.io/agentjoin/kiosk.html?';
    var i2 = document.getElementById('videoID').value;
    f.action = i1 + i2;
    return true;
